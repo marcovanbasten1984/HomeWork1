2592d89c1970b862d79e9f8b4b46021dfcf34112
marcovanbasten1984 <32526158+marcovanbasten1984@users.noreply.github.com>
2021-07-26 22:22:01 +0000

    Update Contents.swift

--- HomeWork1.playground/Contents.swift
+++ HomeWork1.playground/Contents.swift
@@ -1,5 +1,3 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
 // TASK 1
@@ -17,43 +15,48 @@ import UIKit
    И допустим, что количество сотрудников меньше 30, т.е. съедят они не все
 */
 
-let numberOfWworkers: Int = 27
+/// Очепятка: `Workers`
+// Исправляю
+let numberOfWorkers: Int = 27
 
 let pizzaCount = 5
-let beerCount = 10
+/// Тут скорее `beerBottles`
+// Исправляю
+let beerBottles = 10
+
 let numberOfPeoplePerPizza = 6
 let numberOfPeoplePerBeer = 3
+let plannedNumberOfWorkers = 30
 
 /// Опять же, комментарии это хорошо, но читаемость кода ухудшается, когда вместо, например
 /// let pizzaPieces = pizzaCount * numberOfPeoplePerPizza
 /// видишь 5 * 6.
 /// Но, ход мыслей правильный
 
-// столько человек можно накормить пятью пиццами
-5 * 6
-
-// столько человек можно напоить шестью пивами
-10 * 3
-
 /// Форматирование кода: делай отступы для кода в блоках (между фигурных скобок `{...}` )
 /// `else` идет на одной строке после `}`
-if numberOfWworkers > 30 {
+
+/// Не сильно понимаю, почему сравниваем с `30`
+/// Если это какое-то важное значение - создай для него константу.
+// Исправляю выше let plannedNumberOfWorkers = 30. Как следует из условия, заказ расчитан на 30 человек.
+if numberOfWorkers > plannedNumberOfWorkers {
     print ("На всех не хватит")
- } else {
-    print ("Устроителю вечеринки достанется кусочков пиццы: \((pizzaCount*numberOfPeoplePerPizza - numberOfWworkers)*2)")
-    print ("Устроителю вечеринки достанется литров пива: \(Double(beerCount*numberOfPeoplePerBeer - numberOfWworkers)*0.5)")
- }
+/// Скобки уехали на 1 пробел вправо :)
+} else {
+    print ("Устроителю вечеринки достанется кусочков пиццы: \((pizzaCount*numberOfPeoplePerPizza - numberOfWorkers)*2)")
+    print ("Устроителю вечеринки достанется литров пива: \(Double(beerBottles*numberOfPeoplePerBeer - numberOfWorkers)*0.5)")
+}
 
 // using operator %
 
-if numberOfWworkers > 30 {
+if numberOfWorkers > plannedNumberOfWorkers {
     print ("На всех не хватит")
 } else {
-    print ("Устроителю вечеринки достанется кусочков пиццы (используя оператор %): \((pizzaCount*numberOfPeoplePerPizza - (numberOfWworkers % 30))*2)")
-    print ("Устроителю вечеринки достанется литров пива(используя оператор %): \(Double(beerCount*numberOfPeoplePerBeer - (numberOfWworkers % 30))*0.5)")}
+    print ("Устроителю вечеринки достанется кусочков пиццы (используя оператор %): \((pizzaCount*numberOfPeoplePerPizza - (numberOfWorkers % plannedNumberOfWorkers))*2)")
+    print ("Устроителю вечеринки достанется литров пива(используя оператор %): \(Double(beerBottles*numberOfPeoplePerBeer - (numberOfWorkers % plannedNumberOfWorkers))*0.5)")}
 
-/// По еде - правильно.
-/// Но, не посчитал по напиткам.
+
+/// Теперь правильно посчитал :)
 
 
 // TASK 2
@@ -66,6 +69,7 @@ let halfMoneyFriend1: Int = 250
 let halfMoneyFriend2: Int = 200
 */
 
+var totalBudget: Int = 0
 let halfMoneyMine: Int = 300
 let halfMoneyFriend1: Int = 254
 let halfMoneyFriend2: Int = 253
@@ -78,7 +82,7 @@ if halfMoneyMine < halfMoneyFriend1 && halfMoneyMine < halfMoneyFriend2 {
     /// Прочитай еще раз задание. Ориентируемся на меньшую из половин сбережений,
     /// а не тратим сбережения того, у кого их больше :)
    print ("У меня денег меньше всего")
-   var totalBudget = halfMoneyMine * 3
+   totalBudget = halfMoneyMine * 3
    print ("Все скинулись и бюджет составил: \(totalBudget)")
 
 /// Кажется, что здесь условие избыточное.
@@ -87,30 +91,48 @@ if halfMoneyMine < halfMoneyFriend1 && halfMoneyMine < halfMoneyFriend2 {
     
 } else if halfMoneyFriend1 < halfMoneyFriend2 {
     print ("Меньше всего денег у первого друга")
-    var totalBudget = halfMoneyFriend1 * 3
+    totalBudget = halfMoneyFriend1 * 3
     print ("Все скинулись и бюджет составил: \(totalBudget)")
 } else {
     print ("Меньше всего денег у второго друга")
-    var totalBudget = halfMoneyFriend2 * 3
+    totalBudget = halfMoneyFriend2 * 3
     print ("Все скинулись и бюджет составил: \(totalBudget)")
 }
 
-/// Ход мыслей правильный: находим половины и сравниваем.
-/// Но, саму сумму так и не посчитал.
+/// Сумму посчитал правильно в этот раз, молодец!
+/// Только выше переменная `totalBudget` объявляется в каждом из блоков.
+/// Можно было либо создать ее до `if`, или внутри блоков делать `let`
+/// Xcode показывает warning желтым цветом и предлагает пофиксить :)
+// Вроде исправил
 
 
 // TASK 3
 
 /// Для данной константы есть более подходящее название: `grossSalary`
 //let greySalary: Double = 455.17
-let nettSalary: Double = 1110.17
+
+/// Наоборот :)
+/// Это `grossSalary`, а посчитать нужно `netSalary`
+
+
+let grossSalary: Double = 1110.25
+
 /// Совершенно верно
 // Приведение к типу Int отбрасывает дробное значение, поэтому для округления до целого добавляем единицу
 
-/// Для данной константы есть более подходящее название: `nettSalary`
-var grossSalary = Int (nettSalary - (nettSalary * 0.44)+1)
+/// Неправильно тебе подсказал, здесь `netSalary`
+/// Ставим пролебы вокруг арифметических операторов ` + `
 
-print("Зарплата на руки: \(grossSalary)")
+var netSalary = Int (grossSalary - (grossSalary * 0.44) + 1)
 
+print("Зарплата на руки: \(netSalary)")
 
-
+/// Третье сделал, вроде, правильно, но округление не всегда правильно работать будет.
+/// Если `salary - salary * 0.44` будет целым числом, например:
+/// gross: 1000
+/// net: 560
+/// Округлять не нужно, потому что целое число получается
+/// А у тебя получится 561 :)
+/// Но это такое, уже нюансы.
+/// Можно было этого избежать добавив проверку на то, получилось ли число целым,
+/// с помощью if или поигравшись с преобразованием типов
